f244c1009be0cbb58d68b73d2e8308b16844b972
Ivan Paulouski <ipaulouski@Thunderhead.com>
2015-08-13 12:37:08 +0300

    Generate skill group controller

---
+++ app/controllers/skill_groups_controller.rb
@@ -0,0 +1,2 @@
+class SkillGroupsController < ApplicationController
+end
--- app/models/skill_group.rb
+++ app/models/skill_group.rb
@@ -2,7 +2,6 @@ class SkillGroup < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
   has_many :skills, foreign_key: 'skill_id',  class_name: 'SkillGroupToSkillRelationship'
-
   has_many :report_templates, foreign_key: 'skill_id',  class_name: 'ReportTemplateToSkillGroupRelationship'  , dependent: :destroy
 
 end
