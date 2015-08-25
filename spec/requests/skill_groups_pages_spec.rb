ca4c53851ac72b54c38db0656ba9d026ff653e4a
Ivan Paulouski <ipaulouski@iba.by>
2015-08-25 15:54:24 +0300

    Add some RSPEC tests

---
+++ spec/requests/skill_groups_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "skill_groups" do
+
+	subject { page }
+	
+	describe "skill_groups#index" do
+		let(:user) { FactoryGirl.create(:user) }
+		before(:each) do
+			sign_in user
+			visit skill_groups_path
+		end
+
+		it { should have_title('Skills Groups') }
+    	it { should have_content('All skill groups') }
+    	it { should have_link('Create')}
+
+    	describe "display all skill groups" do
+    		before(:all) { FactoryGirl.create(:skill_group) }
+      		after(:all)  { SkillGroup.delete_all }
+
+      		it { should have_selector('table tbody tr:nth-of-type(1) td:nth-of-type(1)', text: 'Skill group 1')}
+      		it { should have_selector('table tbody tr:nth-of-type(1) td:nth-of-type(2)', text: 'Skill group 1 short description.')}
+    	end
+	end
+end
