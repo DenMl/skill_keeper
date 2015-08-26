2b7176e0214fdae4f1076311eec4012846ca3310
DUrayev <DenisUraev@gmail.com>
2015-08-26 18:23:26 +0300

    Change SkillGroup to Skill association

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,8 @@ class Skill < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
-  has_many :skill_groups, foreign_key: 'skill_id',  class_name:  'SkillGroupToSkillRelationship', dependent: :destroy
+  has_many :skill_group_to_skill_relationships, dependent: :destroy
+  has_many :skill_groups, through: :skill_group_to_skill_relationships
   has_many :candidates, foreign_key: 'skill_id',  class_name:  'CandidateToSkillRelationship', dependent: :destroy
 
 end
