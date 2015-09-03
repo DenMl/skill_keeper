96614974f2e9236c64dd1ac827ca5c9a2dd367c8
Ivan Paulouski <ipaulouski@iba.by>
2015-09-03 15:58:22 +0300

    More suitable cases

--- spec/requests/skill_groups_pages_spec.rb
+++ spec/requests/skill_groups_pages_spec.rb
@@ -1,46 +1,62 @@
 require 'spec_helper'
 require 'capybara'
 
-describe 'Skill Group page' do
+describe 'skill_group' do
 
+  subject { page }
   let!(:user) {FactoryGirl.create(:user)}
   let!(:skill_group) {FactoryGirl.create(:skill_group)}
 
   before(:each) do
-    #FactoryGirl.create(:skill_group)
     sign_in user
     visit skill_groups_path
   end
 
-  it 'have appropriate page structure ' do
-    page.should have_title('Skills Groups')
-    page.should have_content('All skill groups')
-    page.should have_link('Create')
+  describe 'index.html.erb' do
+    it do
+      should have_title('Skills Groups')
+      should have_content('All skill groups')
+      should have_link('Create')
+    end
   end
 
-  it 'create skill group item', js: true do
+  describe '.create', js: true do
+    before do
       click_on 'Create'
       fill_in 'skill_group_name', with: 'Fake Skill Group'
       fill_in 'skill_group_description', with: 'Fake Skill Group description'
       click_on 'Accept'
-      page.should have_content 'Fake Skill Group'
-      page.should have_content 'Fake Skill Group description'
+    end
+    it do
+      should have_content 'Fake Skill Group'
+      should have_content 'Fake Skill Group description'
+    end
   end
 
-  it 'delete skill group item', js: true do
-    find(:css, 'table tbody tr:nth-of-type(1)').find_link('Delete').click
-    click_on 'Accept'
-    page.should_not have_content 'Skill Group 2'
-    page.should_not have_content 'Skill Group 2 short description'
+  describe '.delete', js: true do
+    before do
+      find(:css, 'table tbody tr:nth-of-type(1)').find_link('Delete').click
+      click_on 'Accept'
+      sleep 1
+    end
+    it do
+      within('.table') do
+        should_not have_link 'Delete'
+        should_not have_link 'Edit'
+      end
+    end
   end
 
-  it 'edit skill group item', js: true do
-    find(:css, 'table tbody tr:nth-of-type(1)').find_link('Edit').click
-    fill_in 'skill_group_name', with: 'Edited Skill Group'
-    fill_in 'skill_group_description', with: 'Edited Skill Group description'
-    click_on 'Accept'
-    page.should have_content 'Edited Skill Group'
-    page.should have_content 'Edited Skill Group description'
+  describe '.edit', js: true do
+    before do
+      find(:css, 'table tbody tr:nth-of-type(1)').find_link('Edit').click
+      fill_in 'skill_group_name', with: 'Edited Skill Group'
+      fill_in 'skill_group_description', with: 'Edited Skill Group description'
+      click_on 'Accept'
+    end
+    it do
+      should have_content 'Edited Skill Group'
+      should have_content 'Edited Skill Group description'
+    end
   end
-
 end
